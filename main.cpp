902b2a0dbee1226178562954b14d9319f3db490e
ClickezIci <clickezici@mail.ru>
2026-09-11 10:50:22 +0200

    110926

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 
 int main(int argc, char* argv[])
 {
+
     std::string language = /*"en";//*/getSystemLanguage();
     summonTerminal(argc, argv);
 
     snakeTerminal(language);
 
     return 0;
+
 }
+
+//Par ClickezIci <3
